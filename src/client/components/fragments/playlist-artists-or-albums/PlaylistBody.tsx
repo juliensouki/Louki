617cc7d8b9ef08369fedadffca3de5fd9ed64f90
Julien Souki-Leon <julien.soukileon@gmail.com>
2020-01-22 12:00:09 +0100

    loading artists and albums

--- src/client/components/fragments/playlist-artists-or-albums/PlaylistBody.tsx
+++ src/client/components/fragments/playlist-artists-or-albums/PlaylistBody.tsx
@@ -8,95 +8,54 @@ import PlaylistBodyMobile from './PlaylistBodyMobile';
 import ResponsiveAdapter from '../../utils/ResponsiveAdapter';
 import CurrentArtistOrAlbum from '../../../store/pages/artistsOrAlbums/CurrentArtistOrAlbum';
 
-const styles = (theme: Theme) => createStyles({
+import { Page } from '../../pages/artists-or-albums/ArtistsOrAlbums';
+import MusicsData from '../../../store/common/MusicsData';
+
+const styles = (theme: Theme) =>
+  createStyles({
     '@global': {
-        '.MuiTableCell-root': {
-            padding: 8,
-        },
+      '.MuiTableCell-root': {
+        padding: 8,
+      },
     },
     root: {
-        marginLeft: 20,
-        width: "calc(100% - 40px)",
-        height: "calc(100% - 280px)",
-        marginTop: 20,
-        [theme.breakpoints.down("xs")]: {
-            height: "calc(100% - 240px)",
-        }
-    },
-    table: {
+      marginLeft: 20,
+      width: 'calc(100% - 40px)',
+      height: 'calc(100% - 280px)',
+      marginTop: 20,
+      [theme.breakpoints.down('xs')]: {
+        height: 'calc(100% - 240px)',
+      },
     },
     rowTitles: {
-        fontWeight: "bolder",
-        textTransform: "uppercase",
-        fontSize: 16,
-        color: theme.palette.primary.main,
+      fontWeight: 'bolder',
+      textTransform: 'uppercase',
+      fontSize: 16,
+      color: theme.palette.primary.main,
     },
     tableRow: {
-        color: theme.palette.primary.main,        
+      color: theme.palette.primary.main,
     },
-});
+  });
 
-interface Props extends WithStyles<typeof styles>
-{
-};
+interface IProps extends WithStyles<typeof styles> {
+  page: Page;
+}
 
 @observer
-class PlaylistBody extends React.Component<Props, NoState>
-{
-  get playlistArtist(): Array<any> 
-  {
-    return [
-      {
-        name: 'Coldplay',
-        numberSongs: '14',
-        duration: '31:42',
-      },
-      {
-        name: 'Radiohead',
-        numberSongs: '19',
-        duration: '43:21',
-      },
-      {
-        name: 'Hans Zimmer',
-        numberSongs: '12',
-        duration: '39:29',
-      },
-    ];
-  }
+class PlaylistBody extends React.Component<IProps, NoState> {
+  render() {
+    const { classes, page } = this.props;
+    const playlist = page == Page.ALBUMS ? MusicsData.allAlbums : MusicsData.allArtists;
 
-  get playlistAlbum(): Array<any> 
-  {
-    return [
-      {
-        title: 'A Rush Of Blood to the Head',
-        artist: 'Coldplay',
-        numberSongs: '4',
-        duration: '13:42',
-      },
-      {
-        title: 'Rainbo',
-        artist: 'Radiohead',
-        numberSongs: '2',
-        duration: '8:42',
-      },
-    ];
-  }
-
-  render()
-  {
-      const { classes } = this.props;
-      const playlist = CurrentArtistOrAlbum.showArtist ? this.playlistArtist : this.playlistAlbum;
-  
-      return (
-          <div className={classes.root}>
-              <ResponsiveAdapter mobile={
-                  <PlaylistBodyMobile playlist={playlist}/>
-              }
-              desktop={
-                  <PlaylistBodyDesktop playlist={playlist}/>
-              }/>
-          </div>
-      );
+    return (
+      <div className={classes.root}>
+        <ResponsiveAdapter
+          mobile={<PlaylistBodyMobile playlist={playlist} />}
+          desktop={<PlaylistBodyDesktop playlist={playlist} page={page} />}
+        />
+      </div>
+    );
   }
 }
 
