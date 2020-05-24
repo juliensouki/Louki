d1d3e78b07007cc0685e4388688403424fda9d45
Julien Souki-Leon <julien.soukileon@gmail.com>
2020-05-24 18:40:18 +0200

    refact: select as new component in utils/

--- src/client/components/pages/settings/Settings.tsx
+++ src/client/components/pages/settings/Settings.tsx
@@ -11,9 +11,8 @@ import CloseIcon from '@material-ui/icons/Close';
 import CheckIcon from '@material-ui/icons/Check';
 
 import { Grid, Button, Typography, Switch } from '@material-ui/core/';
-import InputBase from '@material-ui/core/InputBase';
-import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Select from '../../utils/Select';
 
 import SimpleHeader from '../../layout/SimpleHeader';
 import ConfirmModal from '../../utils/ConfirmModal';
@@ -77,41 +76,6 @@ const styles = (theme: Theme) =>
       fontSize: '2rem',
     },
   });
-
-const BootstrapInput = withStyles(theme => ({
-  root: {
-    'label + &': {
-      marginTop: theme.spacing(3),
-    },
-  },
-  input: {
-    borderRadius: 4,
-    position: 'relative',
-    backgroundColor: theme.palette.background.default,
-    color: theme.palette.primary.main,
-    fontWeight: 'bold',
-    fontSize: '1.5rem',
-    padding: '10px 26px 10px 12px',
-    transition: theme.transitions.create(['border-color', 'box-shadow']),
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-    '&:focus': {
-      borderRadius: 4,
-      borderColor: '#80bdff',
-      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-    },
-  },
-}))(InputBase);
 
 @observer
 class Settings extends React.Component<WithStyles & RouteComponentProps, NoState> {
@@ -256,12 +220,10 @@ class Settings extends React.Component<WithStyles & RouteComponentProps, NoState
             <Grid className={classes.gridContainer} item container direction='row' justify='space-between'>
               <Typography className={classes.text}>{T.language} : </Typography>
               <Select
-                id='demo-customized-select-native'
                 value={SettingsForm.language}
                 onChange={event => {
                   SettingsForm.setLanguage(event.target.value as Language);
                 }}
-                input={<BootstrapInput />}
               >
                 <MenuItem className={classes.text} value={Language.French}>
                   Français
