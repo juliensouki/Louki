3b3312399a3f5d37301ccf6b1d79be130269af57
Julien Souki-Leon <julien.soukileon@gmail.com>
2020-05-30 04:27:23 +0200

    fix: handle empty artists and albums

--- src/client/components/playlist/artists-or-albums-playlist-layout/PlaylistBodyMobile.tsx
+++ src/client/components/playlist/artists-or-albums-playlist-layout/PlaylistBodyMobile.tsx
@@ -5,11 +5,10 @@ import { observer } from 'mobx-react';
 import { Theme, createStyles, WithStyles, withStyles } from '@material-ui/core/styles';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
 
-import { IconButton, Grid, Typography } from '@material-ui/core';
+import { Button, Grid, Typography } from '@material-ui/core';
 import { Page } from '../../pages/artists-or-albums/ArtistsOrAlbums';
 import { Artist, Album } from '../../../../shared/LoukiTypes';
-
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import texts from '../../../lang/playlist/artists-or-albums-playlist-layout';
 
 const styles = (theme: Theme) =>
   createStyles({
@@ -52,44 +51,63 @@ class PlaylistBodyMobile extends React.Component<Props & RouteComponentProps, No
     this.props.history.push('/' + page + '/' + item.__id);
   };
 
+  redirectHome = () => {
+    this.props.history.push('/all-musics');
+  };
+
   render() {
     const { classes, playlist } = this.props;
-    return (
-      <div style={{ width: '100%' }}>
-        {playlist.map((row, index) => (
-          <Grid
-            key={index}
-            container
-            direction='row'
-            alignItems='center'
-            justify='space-between'
-            className={classes.songContainer}
-            onClick={() => {
-              this.redirectToSpecificArtistOrAlbum(row);
-            }}
-          >
+    const T = texts.current;
+
+    if (playlist.length == 0) {
+      return (
+        <React.Fragment>
+          <Typography style={{ fontSize: '1.3rem', display: 'inline-block' }}>
+            {this.props.page == Page.ARTISTS ? T.artists.emptyText : T.albums.emptyText}
+          </Typography>
+          <Button className={classes.button} onClick={this.redirectHome}>
+            {this.props.page == Page.ARTISTS ? T.artists.emptyButton : T.albums.emptyButton}
+          </Button>
+        </React.Fragment>
+      );
+    } else {
+      return (
+        <div style={{ width: '100%' }}>
+          {playlist.map((row, index) => (
             <Grid
-              item
+              key={index}
               container
-              direction='column'
-              alignItems='flex-start'
+              direction='row'
+              alignItems='center'
               justify='space-between'
-              className={classes.songInfoContainer}
+              className={classes.songContainer}
+              onClick={() => {
+                this.redirectToSpecificArtistOrAlbum(row);
+              }}
             >
-              <Grid item style={{ width: '100%' }}>
-                <Typography className={classes.songName}>{row.title || row.name}</Typography>
+              <Grid
+                item
+                container
+                direction='column'
+                alignItems='flex-start'
+                justify='space-between'
+                className={classes.songInfoContainer}
+              >
+                <Grid item style={{ width: '100%' }}>
+                  <Typography className={classes.songName}>{row.title || row.name}</Typography>
+                </Grid>
+                <Grid item>
+                  <Typography className={classes.artistName}>{row.artist}</Typography>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Typography className={classes.artistName}>{row.artist}</Typography>
+              <Grid item style={{ width: 'auto' }}>
+                <Typography className={classes.songDuration}>{row.duration}</Typography>
               </Grid>
             </Grid>
-            <Grid item style={{ width: 'auto' }}>
-              <Typography className={classes.songDuration}>{row.duration}</Typography>
-            </Grid>
-          </Grid>
-        ))}
-      </div>
-    );
+          ))}
+        </div>
+      );
+    }
   }
 }
 
